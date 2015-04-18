d0090d6aab4da737199b2d0e80e4b461804f02d9
Evgenia <evgenia_@email.ua>
2015-04-18 13:08:55 +0300

    Day27 fragments

---
+++ app/src/main/java/android/android_lessons/belkin/com/day27/Fragment2.java
@@ -0,0 +1,22 @@
+package android.android_lessons.belkin.com.day27;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Belkin on 18.04.2015.
+ */
+public class Fragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        View rootView = inflater.inflate(R.layout.fragment2, container, false);
+        return rootView;
+    }
+}
